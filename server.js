c8f85aa5ff2f99fa23228d1ceda9a91beafa8309
darkrazor99 <tarektheman99@gmail.com>
2021-10-07 12:06:32 +0200

    feat: Started up the server and added the routes

--- server.js
+++ server.js
@@ -1,22 +1,52 @@
 // Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
 // Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Dependencies */
-/* Middleware*/
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+/* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
+app.use(express.static('website'));
 
 // Spin up the server
+const port = 3000;
+
+const server = app.listen(port, callBack);
 // Callback to debug
+function callBack(){
+    console.log(`server is running on localhost:${port}`);
+}
 
 // Initialize all route with a callback function
+app.get('/all', getData);
 
 // Callback function to complete GET '/all'
+function getData(request, response){
+    response.send(projectData);
+}
 
 // Post Route
-  
+app.post('/add', addData);
+
+// Callback function to complete post '/add'
+function addData(request, response){
+    const newEntity = {
+        temperature: request.temperature,
+        date: request.date,
+        userResponse: request.ueserResponse
+    }
+}
